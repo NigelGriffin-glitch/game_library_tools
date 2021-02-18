903cd454cd16bcdff67f11bd4481417c6825aa06
NigelGriffin-glitch <70772593+NigelGriffin-glitch@users.noreply.github.com>
2021-02-18 14:28:18 -0500

    8b roll_dice

--- dice_roll.py
+++ dice_roll.py
@@ -1,9 +1,20 @@
 # dice rolling library, nigel griffin, febuary 18, 2021, 1:52pm
 
 import random
+import time
 
 # d4 simulator
 def roll_d4(num_roll): # num_roll is an argument
     rolls = 0
-    sum = 0
+    the_sum = 0
+
+while rolls < num_roll:
+    result = random.randint(1, 4)
+    print(f"homie u rolled a {result}.\n")
+    rolls += 1
+    sum += result
+print(f"")
+
+roll_d4(4)
+
 
